1644422999cb652ef0f149a07ca113aa6134a9c4
Carl Hu <hy19900815work@126.com>
2017-11-11 14:44:18 +0800

    sentence rotates

--- RotateString.cpp
+++ RotateString.cpp
@@ -1,5 +1,16 @@
-#include <stdio.h>
+/*
+* @Author: Carl Hu
+* @Date:   2017-11-11 14:21:27
+* @Last Modified by:   Carl Hu
+* @Last Modified time: 2017-11-11 14:40:02
+* @使用三步翻转法实现字符串的转转
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+//字符串翻转
 void ReverseString(char *s, int from, int to)
 {
 	while(from < to)
@@ -12,6 +23,7 @@ void ReverseString(char *s, int from, int to)
 	}
 }
 
+//字符串前面的字符移动到字符串的尾部
 void LeftRotateString(char *s, int length, int middle)
 {
 	middle %= length;
@@ -21,11 +33,54 @@ void LeftRotateString(char *s, int length, int middle)
 
 }
 
+//句子翻转，单词顺序不变
+void ReverseWordSentence(char* s, char* newstr)
+{
+	char oldstr[100];
+	strcpy(oldstr, s);
+	int length = strlen(oldstr);
+	int wordStart = 0;
+	int wordEnd = 0;
+	while(*s)
+	{
+		char t = *s;
+		if (t==32)
+		{
+			ReverseString(oldstr,wordStart,wordEnd-1);
+			wordStart = ++wordEnd;
+		}
+		else if(wordEnd==length-1)
+		{
+			ReverseString(oldstr,wordStart,wordEnd);
+			++wordEnd;
+		}
+		else
+		{
+			++wordEnd;
+		}
+
+		++s;
+	}
+	ReverseString(oldstr, 0, wordEnd-1);
+	strcpy(newstr, oldstr);
+}
+
 int main()
 {
-	char str[] = "HelloWorld";
-	printf("old string is %s\n", str);
-	LeftRotateString(str,10,5);
-	printf("new string is %s\n", str);	
+	char orgStr[] = "abcdef";
+	char words[] = "I am a university student from shanghai city of China";
+
+	printf("origin string:\n%s\n", orgStr);
+	LeftRotateString(orgStr, strlen(orgStr), 3);
+	printf("new string:\n%s\n", orgStr);
+
+	printf("old sentence:\n%s\n", words);
+	char newwords[100];
+	
+	ReverseWordSentence(words,newwords);
+
+	printf("new sentence:\n%s\n",newwords);
+
+	return 0;	
 
 }
